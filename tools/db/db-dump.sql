e9594830ca0bd58ae9f2389f3fc78d20449cceae
Xesenix <pkapalla@xesenix.pl>
2022-11-21 01:53:28 +0100

    build progress in container with data initialization

---
+++ tools/db/db-dump.sql
@@ -0,0 +1,13 @@
+-- Adminer 4.8.1 PostgreSQL 15.1 (Debian 15.1-1.pgdg110+1) dump
+
+\connect "postgres";
+
+DROP TABLE IF EXISTS "test";
+CREATE TABLE "public"."test" (
+    "version" character varying(255) NOT NULL
+) WITH (oids = false);
+
+INSERT INTO "test" ("version") VALUES
+('A');
+
+-- 2022-11-21 00:07:19.501465+00
